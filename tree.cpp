8e6e6487ff622100a88b4a151b025df7199ccc18
whosefriendA <2653344989@qq.com>
2024-09-08 18:59:45 +0800

    new

---
+++ tree.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+using std::string;
+//顺序结构树
+typedef struct Treenode{
+    string data;
+    int parent;
+}Treenode;
+typedef struct Tree{
+    Treenode nodes[100];
+    int r,n;
+}Tree;
+int main(){
+
+}
